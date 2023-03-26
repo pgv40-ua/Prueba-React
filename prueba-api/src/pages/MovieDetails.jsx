c2cf44f82a27f6589be5a9aa745255099535edfb
Pablo <esorpius12@gmail.com>
2023-03-26 19:10:59 +0200

    retocado MovieDetails

--- prueba-api/src/pages/MovieDetails.jsx
+++ prueba-api/src/pages/MovieDetails.jsx
@@ -4,16 +4,17 @@ import { get } from "../data/httpClient"
 import { getMovieImage } from "../utils/getMovieImg"
 
 export function MovieDetails(){
-    const {movieId} = useParams
-    const [movie, setMovie] = useState(null)
-    const imageUrl = getMovieImage()
+    const {movieId} = useParams();
+    const [movie, setMovie] = useState([]);
     useEffect(()=>{
         get("/movie/" + movieId).then((data)=>{
             setMovie(data)
         })
     },[movieId])
-   
+
+    const imageUrl = getMovieImage(movie.poster_path, 500)
+
    return(<div>
-        <img src="" alt="" />
+        <img src={imageUrl} />
    </div>)
 }
--- prueba-api/src/utils/getMovieImg.jsx
+++ prueba-api/src/utils/getMovieImg.jsx
@@ -4,5 +4,3 @@ import placeholder from "../img/placeholder.png"
 export function getMovieImage(path, width){
     return path? `https://image.tmdb.org/t/p/w${width}${path}`:placeholder 
 }
-
-//const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;

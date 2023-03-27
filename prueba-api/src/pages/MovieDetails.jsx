00fc7df6b1274ae7de688a45bc5d4623f194aa9e
pgv40-ua <esorpius12@gmail.com>
2023-03-27 17:38:39 +0200

    Proyecto finalizado

--- prueba-api/src/pages/MovieDetails.jsx
+++ prueba-api/src/pages/MovieDetails.jsx
@@ -1,20 +1,40 @@
-import React, { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import { get } from "../data/httpClient"
-import { getMovieImage } from "../utils/getMovieImg"
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { get } from "../data/httpClient";
+import { getMovieImage } from "../utils/getMovieImg";
+import "../pages/MovieDetails.css"
 
-export function MovieDetails(){
-    const {movieId} = useParams();
-    const [movie, setMovie] = useState([]);
-    useEffect(()=>{
-        get("/movie/" + movieId).then((data)=>{
-            setMovie(data)
-        })
-    },[movieId])
+export function MovieDetails() {
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState([]);
+  const [genero, setGenero] = useState([]);
 
-    const imageUrl = getMovieImage(movie.poster_path, 500)
+  useEffect(() => {
+    get("/movie/" + movieId).then((data) => {
+      setMovie(data);
+      setGenero(data.genres[0]);
+    });
+  }, [movieId]);
 
-   return(<div>
-        <img src={imageUrl} />
-   </div>)
+  const imageUrl = getMovieImage(movie.poster_path, 500);
+
+  return (
+    <div className="detailsContainer">
+      <img className="col movieImage" src={imageUrl} alt={movie.title} />
+      <div className="col movieDetails">
+        <p className="titulo2">
+          <strong>Título: </strong>
+          {movie.title}
+        </p>
+        <p className="genero">
+          <strong>Genero: </strong>
+          {genero.name}
+        </p>
+        <p className="descripcion">
+          <strong>Descripción: </strong>
+          {movie.overview}
+        </p>
+      </div>
+    </div>
+  );
 }

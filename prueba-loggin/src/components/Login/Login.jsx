ea16f1b6241c3ec27fcd7407e5bd7b5c99bffaaa
pgv40-ua <esorpius12@gmail.com>
2023-03-28 19:20:54 +0200

    Login finalizado

--- prueba-loggin/src/components/Login/Login.jsx
+++ prueba-loggin/src/components/Login/Login.jsx
@@ -1,18 +1,58 @@
 import styles from "./Login.module.css";
-import { Link } from "react-router-dom";
+import { InputControl } from "../InputControl/InputControl";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
 
 export function Login() {
+  const navigate = useNavigate();
+  const [values, setValues] = useState({ email: "", password: "" });
+  const [errorMsg, setErrorMsg] = useState([]);
+  const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
+  const Iniciar = () => {
+    if (!values.email || !values.password) {
+      setErrorMsg("Datos incompletos");
+      return;
+    }
+    setErrorMsg("");
+    setSubmitButtonDisabled(true);
+    signInWithEmailAndPassword(auth, values.email, values.password)
+      .then(async (res) => {
+        setSubmitButtonDisabled(false);
+        navigate("/");
+      })
+      .catch((err) => {
+        setSubmitButtonDisabled(false);
+        setErrorMsg(err.message);
+      });
+  };
   return (
     <div className={styles.container}>
       <div className={styles.innerBox}>
-        <h1 className={styles.heading}>Login </h1>
+        <h1 className={styles.heading}>Login</h1>
+        <InputControl
+          label="Email"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, email: event.target.value }))
+          }
+          placeholder="Ingrese su correo"
+        />
+        <InputControl
+          label="Contraseña"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, password: event.target.value }))
+          }
+          placeholder="Ingrese su contraseña"
+        />
+
         <div className={styles.footer}>
-          <b className={styles.error}>Error</b>
-          <button>Login btn</button>
+          <b className={styles.error}>{errorMsg}</b>
+          <button onClick={Iniciar} disabled={submitButtonDisabled}>Iniciar sesión</button>
           <p>
-            Debes crear una cuenta
+            Crear cuenta
             <span>
-              <Link to={"/signup"}> click aquí</Link>
+              <Link to="/signup"> click aquí</Link>
             </span>
           </p>
         </div>
